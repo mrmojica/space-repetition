2547d51c61eef6eb348849c3b98ee4602eedd3d9
R. Terrado <r.terrado@gmail.com>
2016-10-04 07:45:20 -0700

    Connected frontend with backend

--- server/words.js
+++ server/words.js
@@ -1,18 +1,53 @@
 var words = [{
-	id: 0, 
+	id: 0,
 	french: 'chat',
 	english: 'cat'
 },
 {
-	id: 1, 
+	id: 1,
 	french: 'chien',
 	english: 'dog'
 },
 {
-	id: 2, 
+	id: 2,
 	french: 'renard',
 	english: 'fox'
-}];
+},
+{
+	id: 3,
+	french: 'tigre',
+	english: 'tiger'
+},
+{
+	id: 4,
+	french: 'oiseau',
+	english: 'bird'
+},
+{
+	id: 5,
+	french: 'ours',
+	english: 'bear'
+},
+{
+	id: 6,
+	french: 'lapin',
+	english: 'rabbit'
+},
+{
+	id: 7,
+	french: 'grenouille',
+	english: 'frog'
+},
+{
+	id: 8,
+	french: 'loup',
+	english: 'wolf'
+},
+{
+	id: 9,
+	french: 'tortue',
+	english: 'turtle'
+},];
 
 
 module.exports = words;
